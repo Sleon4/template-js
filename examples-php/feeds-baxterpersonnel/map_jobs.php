68f80d38200aa91c3dec7729ea719b6b5f275bc4
Sergio León <sergioleon4004@hotmail.com>
2022-06-16 12:07:29 -0500

    nuevo ejemplo

---
+++ examples-php/feeds-baxterpersonnel/map_jobs.php
@@ -0,0 +1,17 @@
+<?php
+
+$row = $j;
+
+if (((string) $row['salary_frequency']) === 'hour' && preg_match("/9/i", trim((string) strtolower($row["id"])))) {
+    $job = [
+        'title' => trim((string) $row['title']),
+        'reqid' => (string) $row['id'],
+        'location' => trim((string) $row['location']),
+        'url' => trim((string) $row['url']),
+        'dateposted_raw' => date('m/d/Y', strtotime((string) $row['date'])),
+        'html' => (string) $row['description'],
+        'jobdesc' => strip_tags((string) $row['description']),
+        'source_location' => trim((string) $row['location']),
+        'temp' => 1
+    ];
+}
