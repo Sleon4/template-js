6fff8b81dc878f7438919051867dc509b7c79d19
Sergio León <sergioleon4004@hotmail.com>
2022-07-06 10:14:13 -0500

    updated templates

--- templates/date-format/calculate-days.js
+++ templates/date-format/calculate-days.js
@@ -2,9 +2,3 @@ const calculateDates = (date, cont) => {
     date.setDate(date.getDate() + cont);
     return date;
 };
-
-console.log(
-    calculateDates(
-        new Date(), -Math.abs(10)
-    ).toLocaleDateString('en-US')
-);
