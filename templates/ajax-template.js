100bcc40b6a4e079cb3de7245f5dd0723b1ac863
Sergio León <sergioleon4004@hotmail.com>
2022-07-15 09:56:23 -0500

    nuevo template

---
+++ templates/ajax-template.js
@@ -0,0 +1,20 @@
+(() => {
+    const jobs = [];
+    let cont = 0, limit = 0, i = 0, strTotal = '';
+
+    $.ajax({
+        url: ``,
+        type: 'POST',
+        dataType: 'json',
+        // data: JSON.stringify(data),
+        async: false,
+        headers: {
+            "accept": "application/json, text/plain, */*",
+            "content-type": "application/json;charset=UTF-8"
+        },
+        success: res => msg(res),
+        error: err => msg(err)
+    });
+
+    return { jobs: jobs };
+});
